17a04b08f7293721912fcc54072bb421a82a3cbd
James Suess <jsuess@James-Suesss-MBP-8371.local>
2015-09-10 11:53:16 -0500

    Adding better response returns, formatting to PSR2.

--- src/Tms/Resource/BaseMessage.php
+++ src/Tms/Resource/BaseMessage.php
@@ -3,7 +3,8 @@ namespace Tms\Resource;
 
 use \Tms\Client;
 
-abstract class BaseMessage {
+abstract class BaseMessage
+{
     public $recipients;
 
     protected $client;
@@ -11,32 +12,46 @@ abstract class BaseMessage {
     protected $response;
     protected $message_type;
 
-    public function __construct( Client $client ) {
+    public function __construct(Client $client)
+    {
         $this->client = $client;
         $this->recipients = new Recipients();
     }
 
-    public function build( array $message ) {
-        $this->message = array_merge( $message );
+    public function build(array $message)
+    {
+        $this->message = array_merge($message);
     }
 
-    public function get() {
+    public function get()
+    {
         return $this->response;
     }
 
-    public function post() {
-        $ch = curl_init( $this->client->api_root . $this->message_type );
-        $post_fields = json_encode( array_merge( $this->message, $this->recipients->get() ) );
+    public function post()
+    {
+        $ch = curl_init($this->client->api_root . $this->message_type);
+        $post_fields = json_encode(array_merge($this->message, $this->recipients->get()));
 
-        curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'POST' );
-        curl_setopt( $ch, CURLOPT_HTTPHEADER, array(
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             'X-AUTH-TOKEN: ' . $this->client->auth_token,
             'Content-Type: application/json;',
             'charset: utf-8',
         ));
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-        curl_setopt( $ch, CURLOPT_POSTFIELDS, $post_fields );
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
 
-        $this->response = curl_exec( $ch );
+        $response = curl_exec($ch);
+
+        if ($response === false) {
+            $error_response = new \stdClass();
+            $error_response->errors = new \stdClass();
+            $error_response->errors->curl = array(curl_error($ch));
+
+            return $this->response = $error_response;
+        }
+
+        return $this->response = json_decode(curl_exec($ch));
     }
 }
